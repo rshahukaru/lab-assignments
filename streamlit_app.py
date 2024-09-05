2f3f6c331fa8456d06fa85e49235a8ed5bf12e4d
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-05 14:53:06 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 lab_01_page = st.Page("lab-01.py", title="Lab 01")
-lab_02_page = st.Page("lab-02.py", title="Lab 01")
+lab_02_page = st.Page("lab-02.py", title="Lab 02")
 
 pg = st.navigation([lab_01_page, lab_02_page])
 st.set_page_config(page_title="Multi Page Streamlit App")
