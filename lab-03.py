7e2e519269317b66295837e71b583472acc411c9
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-12 14:38:50 +0000

    commit

---
+++ lab-03.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from openai import OpenAI
+
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+# Create an OpenAI client.
+client = OpenAI(api_key=openai_api_key)
+
+# Title
+st.title("📄 LAB 03 - Building Chat Bot :blue[(SUID: 226494782)]")
+
+
+# message = st.chat_message("assistant")
+# message.write("Hello Human")
+
+# prompt = st.chat_input("Say something")
+# if prompt:
+#     st.write(f"User has sent the following part: {prompt}")
+
+# with st.chat_message("assistant"):
+#     response = st.stream(response_generator())
+# st.session_state.messages.append({"role": "assistant",
+#                                   "content": response})
+
+
+openAI_model = st.sidebar.selectbox("Which Model?", 
+                                    ("mini", "regular"))
+
+if openAI_model == "mini":
+    model_to_use = "gpt-4o-mini"
+else:
+    model_to_use = "gpt-4o"
+
+# Create an OpenAI client 
+if 'client' not in st.session_state:
+    api_key = st.secrets["OPENAI_API_KEY"]
+    st.session_chat["messages"] = \
+        [{"role": "assistant","content": "How can I help you?"}]
+    
+# History 
+for msg in st.session_state.messages:
+    chat_msg = st.chat_message(msg["role"])
+    chat_msg.write(msg["content"])
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,28 @@
 import streamlit as st
 
-lab_01_page = st.Page("lab-01.py", title="Lab 01")
-lab_02_page = st.Page("lab-02.py", title="Lab 02", default = True)
+# Set up individual pages for each homework
+hw_01_page = st.Page("lab-01.py", title="Lab-01")
+hw_02_page = st.Page("lab-02.py", title="Lab-02")
+hw_03_page = st.Page("lab-03.py", title="Lab-03")
+hw_04_page = st.Page("lab-04.py", title="Lab-04")
+hw_05_page = st.Page("lab-05.py", title="Lab-05")
+hw_06_page = st.Page("lab-06.py", title="Lab-06")
+hw_07_page = st.Page("lab-07.py", title="Lab-07")
+hw_08_page = st.Page("lab-08.py", title="Lab-08")
+hw_09_page = st.Page("lab-09.py", title="Lab-09")
+hw_10_page = st.Page("lab-10.py", title="Lab-10")
+hw_11_page = st.Page("lab-11.py", title="Lab-11")
+hw_12_page = st.Page("lab-12.py", title="Lab-12")
 
-pg = st.navigation([lab_01_page, lab_02_page])
-st.set_page_config(page_title="Multi Page Streamlit App")
+# Navigation setup with all homework pages
+pg = st.navigation([
+    hw_01_page, hw_02_page, hw_03_page, hw_04_page, hw_05_page,
+    hw_06_page, hw_07_page, hw_08_page, hw_09_page, hw_10_page,
+    hw_11_page, hw_12_page
+])
+
+# Configuration of the main app
+st.set_page_config(page_title="HW Manager")
+
+# Running the page navigation
 pg.run()
