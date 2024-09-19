2445fe46178cb4bf04c97bd16b12da35e2ebc0df
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-19 03:06:54 +0000

    c

--- playground_chatgpt.py
+++ playground_chatgpt.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import openai
-from langchain_ollama import Ollama
+from langchain_ollama import OllamaLLM
 import anthropic
 
 # Title of the Streamlit app
