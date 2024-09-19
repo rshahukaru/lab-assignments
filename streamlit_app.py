bfd950ed8e5a3bd5317ca980b837d3fd1013a899
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-19 04:23:15 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ lab_10_page = st.Page("lab-10.py", title="Lab-10")
 lab_11_page = st.Page("lab-11.py", title="Lab-11")
 lab_12_page = st.Page("lab-12.py", title="Lab-12")
 # playground_page = st.Page("playground.py", title="Playground", default=True)
-playground_page = st.Page("playground_chatgpt.py", title="Playground", default=True)
+playground_page = st.Page("playground_2.py", title="Playground", default=True)
 
 
 
