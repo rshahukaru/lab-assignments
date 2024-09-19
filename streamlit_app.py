98b12eb1e314cb7c42bfcf85caec83e7c09b99ac
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-19 15:51:18 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 # Set up individual pages for each homework
 lab_01_page = st.Page("lab-01.py", title="Lab-01")
 lab_02_page = st.Page("lab-02.py", title="Lab-02")
-lab_03_page = st.Page("lab-03.py", title="Lab-03")
-lab_04_page = st.Page("lab-04.py", title="Lab-04", default = True)
+lab_03_page = st.Page("lab-03.py", title="Lab-03", default = True)
+lab_04_page = st.Page("lab-04.py", title="Lab-04")
 lab_05_page = st.Page("lab-05.py", title="Lab-05")
 lab_06_page = st.Page("lab-06.py", title="Lab-06")
 lab_07_page = st.Page("lab-07.py", title="Lab-07")
@@ -13,14 +13,12 @@ lab_09_page = st.Page("lab-09.py", title="Lab-09")
 lab_10_page = st.Page("lab-10.py", title="Lab-10")
 lab_11_page = st.Page("lab-11.py", title="Lab-11")
 lab_12_page = st.Page("lab-12.py", title="Lab-12")
-playground_page = st.Page("playground.py", title="Playground")
-
 
 # Navigation setup with all homework pages
 pg = st.navigation([
     lab_01_page, lab_02_page, lab_03_page, lab_04_page, lab_05_page,
     lab_06_page, lab_07_page, lab_08_page, lab_09_page, lab_10_page,
-    lab_11_page, lab_12_page, playground_page
+    lab_11_page, lab_12_page
 ])
 
 # Configuration of the main app
