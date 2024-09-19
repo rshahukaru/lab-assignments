e2d720bab2132e90b29ad8cdef11e047f0cd866d
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-19 15:04:49 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 lab_01_page = st.Page("lab-01.py", title="Lab-01")
 lab_02_page = st.Page("lab-02.py", title="Lab-02")
 lab_03_page = st.Page("lab-03.py", title="Lab-03")
-lab_04_page = st.Page("lab-04.py", title="Lab-04")
+lab_04_page = st.Page("lab-04.py", title="Lab-04", default = True)
 lab_05_page = st.Page("lab-05.py", title="Lab-05")
 lab_06_page = st.Page("lab-06.py", title="Lab-06")
 lab_07_page = st.Page("lab-07.py", title="Lab-07")
@@ -13,9 +13,7 @@ lab_09_page = st.Page("lab-09.py", title="Lab-09")
 lab_10_page = st.Page("lab-10.py", title="Lab-10")
 lab_11_page = st.Page("lab-11.py", title="Lab-11")
 lab_12_page = st.Page("lab-12.py", title="Lab-12")
-# playground_page = st.Page("playground.py", title="Playground", default=True)
-playground_page = st.Page("playground_2.py", title="Playground", default=True)
-
+playground_page = st.Page("playground.py", title="Playground")
 
 
 # Navigation setup with all homework pages
